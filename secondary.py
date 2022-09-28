b7c46e66f7de9441583aac024aed98bf3dfe05ac
Hochstatter <hocjacp@dunwoody.edu>
2022-09-28 09:56:36 -0500

    Added system to choose whether to include symbols, and what symbols to include

--- secondary.py
+++ secondary.py
@@ -2,7 +2,7 @@ import string
 import random
 import os
 
-symbols = ['!', '?', '$', '&', '#', '@']  
+  
 cdir = os.getcwd()
 name = 'passwords.txt'
 
@@ -11,6 +11,10 @@ def generate():
     charcount = 0   
     characters = [] 
     isWrite = True
+    symbolsAllowed = False
+    symbols = ['!', '?', '$', '&', '#', '@']
+    symbolsCustom = []
+    useCustom = False
 
     for root, dirs, files in os.walk(cdir):
         for items in files:
@@ -25,20 +29,51 @@ def generate():
     toWrite = []
     os.system('cls')
     passLength = int(input('Enter the length of password you want: '))
-    passType = input('What will this password be used for?: ')  
+    passType = input('What will this password be used for?: ')
     toWrite.append(passType)
-
+    symAllowed = input('Are symbols allowed? (Y/N): ')
+    
+    if symAllowed.upper() == 'Y':
+        symbolsAllowed = True
+        os.system('cls')
+        useCustomQ = input('Would you like to use the default symbols(1), or input a custom list(2)?: ')
+        if useCustomQ == '1':
+            useCustom == False
+        elif useCustomQ == '2':
+            useCustom = True
+            customSymbols = input('Enter your custom list of symbols with no spaces: ')
+            symbolsCustom = customSymbols.split(', ')
+    elif symAllowed.upper() == 'N':
+        symbolsAllowed = False
+    
     while charcount < passLength: 
-        charType = random.randint(1, 3) 
-        if charType == 1:
-            characters.append(str(random.randint(0, 9)))
-            charcount = charcount + 1
-        elif charType == 2:
-            characters.append(random.choice(string.ascii_letters))
-            charcount = charcount + 1
-        elif charType == 3:
-            characters.append(random.choice(symbols))
-            charcount = charcount + 1
+        if symbolsAllowed == True:
+            charType = random.randint(1, 3) 
+            if charType == 1:
+                characters.append(str(random.randint(0, 9)))
+                charcount = charcount + 1
+            elif charType == 2:
+                characters.append(random.choice(string.ascii_letters))
+                charcount = charcount + 1
+            elif charType == 3:
+                if useCustom == False:
+                    characters.append(random.choice(symbols))
+                    charcount = charcount + 1
+                elif useCustom == True:
+                    randomChar = random.choice(customSymbols)
+                    if randomChar == ' ':
+                        print('SPACE ERROR')
+                    else:
+                        characters.append(randomChar)
+                    charcount += 1
+        elif symbolsAllowed == False:
+            charType = random.randint(1, 2) 
+            if charType == 1:
+                characters.append(str(random.randint(0, 9)))
+                charcount = charcount + 1
+            elif charType == 2:
+                characters.append(random.choice(string.ascii_letters))
+                charcount = charcount + 1
 
     finalPass = "".join(characters) 
     toWrite.append(finalPass)
